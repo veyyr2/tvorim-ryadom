9337b74f17899e57865664dde54d7dfccf9a0c69
veyrr2 <gorokhov_d@iuca.kg>
2025-03-29 21:24:24 +0600

    ver 5 true

---
+++ js/dark_theme.js
@@ -0,0 +1,5 @@
+let but_night = document.getElementById('but_night_theme').addEventListener('click', click_but);
+let body = document.body;
+function click_but (){
+    body.classList.toggle('dark_theme');
+}
